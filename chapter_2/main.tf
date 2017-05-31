69613758906bb14c775c74546a84ccf09fde3bab
Kosmas Chatzimichalis <kosmas@mach7x.com>
2017-05-31 13:49:16 +0200

    Deploy a Cluster of Web Servers - no data source

--- chapter_2/main.tf
+++ chapter_2/main.tf
@@ -2,10 +2,10 @@ provider "aws" {
   region        = "eu-central-1"
 }
 
-resource "aws_instance" "example" {
-  ami                     = "ami-060cde69"
+resource "aws_launch_configuration" "example" {
+  image_id                = "ami-060cde69"
   instance_type           = "t2.micro"
-  vpc_security_group_ids  = ["${aws_security_group.instance.id}"]
+  security_groups         = ["${aws_security_group.instance.id}"]
 
   user_data               = <<-EOF
                             #!/bin/bash
@@ -13,9 +13,6 @@ resource "aws_instance" "example" {
                             nohup busybox httpd -f -p "${var.server_port}" &
                             EOF
 
-  tags {
-    Name                  = "terraform-example"
-  }
 }
 
 resource  "aws_security_group" "instance" {
@@ -27,13 +24,27 @@ resource  "aws_security_group" "instance" {
     protocol              = "tcp"
     cidr_blocks           = ["0.0.0.0/0"]
   }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration    = "${aws_launch_configuration.example.id}"
+  availability_zones      = ["eu-central-1a"]
+
+  min_size                = 2
+  max_size                = 10
+
+  tag {
+    key                   = "Name"
+    value                 = "terraform-asg-example"
+    propagate_at_launch   = true
+  }
 }
 
 variable "server_port" {
   description             = "The port the server will use for HTTP requests"
   default                 = 8080
  }
-
-output "public_ip" {
-  value                   = "${aws_instance.example.public_ip}"
-}
